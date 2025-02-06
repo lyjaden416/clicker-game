375cb0c1be4ff45633ae5e706415bca011f86ae9
lyjaden416 <getnoob564@gmail.com>
2025-02-06 11:22:31 -0500

    Add files via upload

---
+++ src/backgrounds.js
@@ -0,0 +1,22 @@
+function wagahai() {
+    document.getElementById("cecilia").style.visibility = "hidden";
+    document.getElementById("biboo").style.visibility = "hidden";
+}
+
+function change() {
+	document.getElementById("wah").style.visibility = "hidden";
+	document.getElementById("cecilia").style.visibility = "visible";
+    document.getElementById("biboo").style.visibility = "hidden";
+}
+
+function back() {
+	document.getElementById("wah").style.visibility = "visible";
+    document.getElementById("cecilia").style.visibility = "hidden";
+    document.getElementById("biboo").style.visibility = "hidden";
+}
+
+function change2() {
+    document.getElementById("wah").style.visibility = "hidden";
+    document.getElementById("cecilia").style.visibility = "hidden";
+    document.getElementById("biboo").style.visibility = "visible";
+}
