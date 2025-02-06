e1a22583f9dea2a7294e0845d8c5d87a7a80a7af
lyjaden416 <getnoob564@gmail.com>
2025-02-06 11:31:09 -0500

    Add files via upload

--- src/backgrounds.js
+++ src/backgrounds.js
@@ -1,22 +1,33 @@
 function wagahai() {
     document.getElementById("cecilia").style.visibility = "hidden";
     document.getElementById("biboo").style.visibility = "hidden";
+    document.getElementById("mumei").style.visibility = "hidden";
 }
 
 function change() {
 	document.getElementById("wah").style.visibility = "hidden";
 	document.getElementById("cecilia").style.visibility = "visible";
     document.getElementById("biboo").style.visibility = "hidden";
+    document.getElementById("mumei").style.visibility = "hidden";
 }
 
 function back() {
 	document.getElementById("wah").style.visibility = "visible";
     document.getElementById("cecilia").style.visibility = "hidden";
     document.getElementById("biboo").style.visibility = "hidden";
+    document.getElementById("mumei").style.visibility = "hidden";
 }
 
 function change2() {
     document.getElementById("wah").style.visibility = "hidden";
     document.getElementById("cecilia").style.visibility = "hidden";
     document.getElementById("biboo").style.visibility = "visible";
+    document.getElementById("mumei").style.visibility = "hidden";
+}
+
+function change3() {
+    document.getElementById("wah").style.visibility = "hidden";
+    document.getElementById("cecilia").style.visibility = "hidden";
+    document.getElementById("biboo").style.visibility = "hidden";
+    document.getElementById("mumei").style.visibility = "visible";
 }
